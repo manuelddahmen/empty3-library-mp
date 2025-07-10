6cd269efdef574ee7d53f6c898015d33f53246ca
Manuel Daniel Dahmen <manuel.dahmen@gmx.com>
2025-07-10 09:28:52 +0200

    SUMMER EDITION 2025 Alpha 2025.6.29
    Signed-off-by: Manuel Daniel Dahmen <manuel.dahmen@gmx.com>

--- src/main/java/one/empty3/apps/facedetect/video/MovieGeneratorHttpFunction.java
+++ src/main/java/one/empty3/apps/facedetect/video/MovieGeneratorHttpFunction.java
@@ -30,7 +30,7 @@ public class MovieGeneratorHttpFunction implements HttpFunction {
     private static final String CONTENT_TYPE_MPEG = "video/mpeg";
     private static final String CONTENT_DISPOSITION_HEADER = "Content-Disposition";
     private static final String CONTENT_DISPOSITION_VALUE = "attachment; filename=\"generated-movie.mpeg\"";
-    private static final String ALLOWED_ORIGIN = "https://motion-weaver-render-1077189433049.us-central1.run.app";
+    private static final String ALLOWED_ORIGIN = "https://us-central1-studio-6v2lo.cloudfunctions.net/motion-weaver-render";
     @Override
     public void service(HttpRequest request, HttpResponse response) throws IOException {
             String origin = request.getFirstHeader("Origin").orElse(null);
