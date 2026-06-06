4d4fd38bcd85481582720e21b3a369777d8bab4b
Manuel Daniel Dahmen <manuel.dahmen@gmx.com>
2026-06-06 15:02:09 +0200

    Release 2026.4.26-25

--- src/main/java/one/empty3/library/Bezier.java
+++ src/main/java/one/empty3/library/Bezier.java
@@ -33,8 +33,5 @@ package one.empty3.library;
 import one.empty3.libs.Image;
 import one.empty3.library.core.nurbs.*;
 
-public class BSpline extends ParametricCurve {
-private double [] knots;
-private Point3D [] controls;
-
+public class Bezier extends CourbeParametriquePolynomialeBezier {
 }
